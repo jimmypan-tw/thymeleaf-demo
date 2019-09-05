70394809ad2a7bb827092f846002202ea290dbf7
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-09-04 22:28:58 -0700

    first commit

---
+++ src/main/java/com/example/Thymeleaf/model/User.java
@@ -0,0 +1,13 @@
+package com.example.Thymeleaf.model;
+
+public class User {
+    public Integer id;
+    public String name;
+    public Integer age;
+
+    public User(Integer id, String name, Integer age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
